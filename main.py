18a0c72577f69b92fccf1b4fec22940525b7cbd7
Igor Nekludov <igor.nekludovv@gmail.com>
2025-05-27 19:46:45 +0300

    track hands using mediapipe and flip frame

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import cv2
+import mediapipe as mp
 
 cam = cv2.VideoCapture(0)
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 
-while cam.isOpened():
-    result, frame = cam.read()
-    if not result:
-        print("empty frame")
-        continue
+with mp_hands.Hands(
+        model_complexity=0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+    while cam.isOpened():
+        result, frame = cam.read()
+        if not result:
+            print("empty frame")
+            continue
 
-    cv2.imshow("", frame)
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
 
-    if cv2.waitKey(1) == ord('q'):
-        break
+        cv2.imshow("", cv2.flip(frame, 1))
+        if cv2.waitKey(1) == ord('q'):
+            break
 
 cam.release()
 cv2.destroyAllWindows()
